f4aeba1eb3df2b8308d45dfb93ae71c52b0dc3fe
Gennadii Saltyshchak <GennadyGS@gmail.com>
2016-06-14 13:23:57 +0300

    SyncAnkiWithGooglePhraseBook.ps1 refactoring

--- SyncAnkiWithGooglePhraseBook.ps1
+++ SyncAnkiWithGooglePhraseBook.ps1
@@ -1,18 +1,28 @@
+Function LoadAnkiConfiguration() {
+    . .\AnkiConfig.ps1
+    if (Test-Path ".\AnkiConfig.private.ps1") {
+        . .\AnkiConfig.private.ps1
+    }
+}
+
+Function AddCardsToDeck() {
+    param ($phraseBookJson)
+
+    Foreach($item in $phraseBookJson[2]) {
+        $cardFront = $item[3]
+        $cardBack = $item[4]
+        Write-Output "Adding card $cardFront`:$cardBack"
+        . .\AddToAnki.ps1 $ankiConfig.collectionFilePath $ankiConfig.deckName $cardFront $cardBack
+    }
+}
+
 Push-Location (Split-Path $MyInvocation.MyCommand.Path -Parent)
 
 $phraseBookJson = (& .\ExportGooglePhraseBook.ps1)
 
-. .\AnkiConfig.ps1
-if (Test-Path ".\AnkiConfig.private.ps1") {
-    . .\AnkiConfig.private.ps1
-}
+LoadAnkiConfiguration
 
-Foreach($item in $phraseBookJson[2]) {
-    $cardFront = $item[3]
-    $cardBack = $item[4]
-    Write-Output "Adding card $cardFront`:$cardBack"
-    . .\AddToAnki.ps1 $ankiConfig.collectionFilePath $ankiConfig.deckName $cardFront $cardBack
-}
+AddCardsToDeck $phraseBookJson
 
 . .\SyncAnki.ps1 $ankiConfig.collectionFilePath $ankiConfig.webCredentials.userName $ankiConfig.webCredentials.password
 
