38e4767e0960e5db6b1ced589687620fa7e10f9c
Gennadii Saltyshchak <GennadyGS@gmail.com>
2017-05-03 18:38:14 +0300

    Automatically copy chrome profile if it is not exists in Output directory

--- ExportGooglePhraseBookChrome.ps1
+++ ExportGooglePhraseBookChrome.ps1
@@ -3,5 +3,11 @@ param (
 )
 
 $chromeProfile = "$PSScriptRoot\Output\ChromeProfile\User Data"
-
+if (!(Test-Path $chromeProfile)) {
+    $sourceChromeProfilePath = "$env:LocalAppData\Google\Chrome\User Data\Default"
+    $targetChromeProfilePath = "$chromeProfile\Default"
+	. taskkill /im "chrome.exe" /f /t
+    Write-Output "Copying chrome profile from "$sourceChromeProfilePath" to "$targetChromeProfilePath"..."
+	Copy-Item $sourceChromeProfilePath $targetChromeProfilePath  -Recurse -ErrorAction Stop
+}
 . "$PSScriptRoot\GooglePhrasebookExport\GooglePhraseBookExport.ps1" $chromeProfile $fileName
