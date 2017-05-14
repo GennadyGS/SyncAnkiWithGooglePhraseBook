9eea33afd972196b48d8b0b2f1c51f1daa345392
Gennadii Saltyshchak <GennadyGS@gmail.com>
2017-05-14 12:15:05 +0300

    Adjust default configuration

--- AnkiConfig.ps1
+++ AnkiConfig.ps1
@@ -1,6 +1,6 @@
 $ankiProfileName = "user1"
 @{
-    collectionFilePath = "$([Environment]::GetFolderPath("mydocuments"))\Anki\$ankiProfileName\collection.anki2";
+    collectionFilePath = "$env:AppData\Anki2\$ankiProfileName\collection.anki2";
     deckName = "Default";
     modelNameTemplate = "Basic"
     webCredentials = @{
