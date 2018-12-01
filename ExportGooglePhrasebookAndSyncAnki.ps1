b7d7c6d5e8e73c76bcd3fc39135a3e9753111fa6
Gennadii Saltyshchak <GennadyGS@gmail.com>
2018-12-01 13:23:22 +0200

    Adapt to new GUI

--- ExportGooglePhrasebookAndSyncAnki.ps1
+++ ExportGooglePhrasebookAndSyncAnki.ps1
@@ -1,6 +1,5 @@
 $outputPath = "Output"
 $phraseBookFileName = "$PSScriptRoot\$outputPath\GooglePhrasebook.json"
 
-#. $PSScriptRoot\ExportGooglePhraseBook.ps1 $phraseBookFileName
 . $PSScriptRoot\ExportGooglePhraseBookChrome.ps1 $phraseBookFileName
-. $PSScriptRoot\SyncAnkiWithGooglePhraseBook.ps1 $phraseBookFileName
+#. $PSScriptRoot\SyncAnkiWithGooglePhraseBook.ps1 $phraseBookFileName
