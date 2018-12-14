87c862fd1ae0131ef6db3ce19b1d06083b61de4a
Gennadii Saltyshchak <GennadyGS@gmail.com>
2018-12-14 10:17:54 +0200

    Restore functionality

--- ExportGooglePhrasebookAndSyncAnki.ps1
+++ ExportGooglePhrasebookAndSyncAnki.ps1
@@ -2,4 +2,4 @@ $outputPath = "Output"
 $phraseBookFileName = "$PSScriptRoot\$outputPath\GooglePhrasebook.json"
 
 . $PSScriptRoot\ExportGooglePhraseBookChrome.ps1 $phraseBookFileName
-#. $PSScriptRoot\SyncAnkiWithGooglePhraseBook.ps1 $phraseBookFileName
+. $PSScriptRoot\SyncAnkiWithGooglePhraseBook.ps1 $phraseBookFileName
