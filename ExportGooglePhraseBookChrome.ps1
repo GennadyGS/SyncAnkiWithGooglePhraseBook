4d3f2abec8cbea2199f93630df973bab237b1b54
Gennadii Saltyshchak <GennadyGS@gmail.com>
2020-10-29 10:02:12 +0200

    Pass profile path without "User Data"

--- ExportGooglePhraseBookChrome.ps1
+++ ExportGooglePhraseBookChrome.ps1
@@ -2,10 +2,11 @@ param (
     $fileName
 )
 
-$chromeProfile = "$PSScriptRoot\Output\ChromeProfile\User Data"
-if (!(Test-Path $chromeProfile)) {
+$chromeProfile = "$PSScriptRoot\Output\ChromeProfile"
+$chromeProfileUserData = "$chromeProfile\User Data"
+if (!(Test-Path $chromeProfileUserData)) {
     $sourceChromeProfilePath = "$env:LocalAppData\Google\Chrome\User Data\Default"
-    $targetChromeProfilePath = "$chromeProfile\Default"
+    $targetChromeProfilePath = "$chromeProfileUserData\Default"
 	. taskkill /im "chrome.exe" /f /t
     Write-Output "Copying chrome profile from "$sourceChromeProfilePath" to "$targetChromeProfilePath"..."
 	Copy-Item $sourceChromeProfilePath $targetChromeProfilePath  -Recurse -ErrorAction Stop
