0203130cb70a1f001c33238ddd3fd93b2cba1da4
Gennadii Saltyshchak <GennadyGS@gmail.com>
2025-01-01 15:06:35 +0200

    Update script

--- ExportGooglePhraseBookFromSpreadSheet.ps1
+++ ExportGooglePhraseBookFromSpreadSheet.ps1
@@ -1,8 +1,20 @@
 param (
-    $spreadSheetId,
-    $outputFileName
+    [Parameter(Mandatory=$true)] $spreadSheetId,
+    [Parameter(Mandatory=$true)] $outputFileName
 )
 
-dotnet run --project $PSScriptRoot/src/ExportGooglePhraseBookFromSpreadSheet `
-    -- $spreadSheetId $outputFileName
+$appPathRoot = "$PSScriptRoot/src"
+$exportGooglePhraseBookFromSpreadSheetAppPath =
+    "$appPathRoot/ExportGooglePhraseBookFromSpreadSheet"
+$shareGoogleDriveFileAppPath = "$appPathRoot/ShareGoogleDriveFile"
 
+$credentialFilePath = "$exportGooglePhraseBookFromSpreadSheetAppPath/credential.json"
+$credential = Get-Content $credentialFilePath -Raw | ConvertFrom-Json
+
+Push-Location $shareGoogleDriveFileAppPath
+dotnet run -- -i $spreadSheetId -u $credential.client_email
+Pop-Location
+
+Push-Location $exportGooglePhraseBookFromSpreadSheetAppPath
+dotnet run -- -i $spreadSheetId -o $outputFileName
+Pop-Location
