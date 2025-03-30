56cd15ab6f70de5158b7bfc1cde1d9ee46ec2780
Gennadii Saltyshchak <GennadyGS@gmail.com>
2025-03-30 20:42:20 +0300

    refactor: remove unused code

--- ExportGooglePhrasebookAndSyncAnki.ps1
+++ ExportGooglePhrasebookAndSyncAnki.ps1
@@ -6,4 +6,3 @@ $outputPath = "Output"
 $phraseBookFileName = "$PSScriptRoot/$outputPath/GooglePhrasebook.json"
 
 . $PSScriptRoot\ExportGooglePhraseBookFromSpreadSheet.ps1 $spreadSheetId $phraseBookFileName
-# . $PSScriptRoot\SyncAnkiWithGooglePhraseBook.ps1 $phraseBookFileName
--- SetPythonEnvVars.ps1
+++
@@ -1,2 +0,0 @@
-$env:PYTHONPATH=".\anki"
-$PythonExe="${env:ProgramFiles(x86)}\Python27\python.exe"
