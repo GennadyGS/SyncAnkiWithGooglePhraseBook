388d130d2aef1f4863aaed898e823822e943a457
Gennadii Saltyshchak <GennadyGS@gmail.com>
2025-05-11 19:32:51 +0300

    Populate what-if parameter

--- ExportGooglePhrasebookAndSyncAnkiCustomUrlHandler.ps1
+++ ExportGooglePhrasebookAndSyncAnkiCustomUrlHandler.ps1
@@ -7,4 +7,4 @@ Add-Type -AssemblyName System.Web
 
 $queryString = [System.Web.HttpUtility]::ParseQueryString($url.Split('?')[1])
 $spreadSheetId = $queryString["spreadSheetId"]
-& $PSScriptRoot/ExportGooglePhrasebookAndSyncAnki.ps1 $spreadSheetId
+& $PSScriptRoot/ExportGooglePhrasebookAndSyncAnki.ps1 $spreadSheetId -what-if:${what-if}
