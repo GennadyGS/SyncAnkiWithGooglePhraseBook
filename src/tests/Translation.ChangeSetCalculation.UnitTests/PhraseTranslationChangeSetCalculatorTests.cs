632cc0ba0070dc836ef717cb3186b9b7eca57b96
Gennadii Saltyshchak <GennadyGS@gmail.com>
2025-06-01 18:39:34 +0300

    refactor: refactor to theory

--- src/tests/Translation.ChangeSetCalculation.UnitTests/PhraseTranslationChangeSetCalculatorTests.cs
+++ src/tests/Translation.ChangeSetCalculation.UnitTests/PhraseTranslationChangeSetCalculatorTests.cs
@@ -1,5 +1,6 @@
 ﻿using ChangeSetCalculation.Models;
 using FluentAssertions;
+using TestUtils;
 using Translation.Models;
 using Xunit;
 
@@ -7,53 +8,18 @@ namespace Translation.ChangeSetCalculation.UnitTests;
 
 public sealed class PhraseTranslationChangeSetCalculatorTests
 {
-    [Fact]
     [System.Diagnostics.CodeAnalysis.SuppressMessage(
         "Layout",
         "MEN003:Method is too long",
-        Justification = "Pending")]
-    public void CalculateChangeSet_ReturnsEmpty_WhenNoChanges()
-    {
-        var source = new PhraseTranslation[]
-        {
-            new()
+        Justification = "Declarative code only.")]
+    public static TheoryData<TestCase> GetTestCases() =>
+        TheoryDataBuilder.TheoryData(
+        [
+            new TestCase
             {
-                Source = new Phrase
-                {
-                    Text = "hassle",
-                    LanguageCode = "en",
-                },
-                Target = new Phrase
-                {
-                    Text = "хлопоты",
-                    LanguageCode = "ru",
-                },
-            },
-        };
-        var target = new Dictionary<long, PhraseTranslation>
-        {
-            [1465978079469] = new()
-            {
-                Source = new Phrase
-                {
-                    Text = "hassle",
-                    LanguageCode = "en",
-                },
-                Target = new Phrase
-                {
-                    Text = "стычка",
-                    LanguageCode = "ru",
-                },
-            },
-        };
-
-        var result = PhraseTranslationChangeSetCalculator.CalculateChangeSet(source, target);
-
-        var expectedResult = new ChangeSet<PhraseTranslation, KeyValuePair<long, PhraseTranslation>>
-        {
-            ToAdd = [],
-            ToUpdate = [
-                new UpdatePair<PhraseTranslation, KeyValuePair<long, PhraseTranslation>>(
+                Id = 1,
+                Source =
+                [
                     new()
                     {
                         Source = new Phrase
@@ -67,24 +33,81 @@ public sealed class PhraseTranslationChangeSetCalculatorTests
                             LanguageCode = "ru",
                         },
                     },
-                    KeyValuePair.Create(
-                        1465978079469,
-                        new PhraseTranslation
+                ],
+                Target = new Dictionary<long, PhraseTranslation>
+                {
+                    [1465978079469] = new()
+                    {
+                        Source = new Phrase
                         {
-                            Source = new Phrase
+                            Text = "hassle",
+                            LanguageCode = "en",
+                        },
+                        Target = new Phrase
+                        {
+                            Text = "стычка",
+                            LanguageCode = "ru",
+                        },
+                    },
+                },
+                ExpectedResult = new ChangeSet<PhraseTranslation, KeyValuePair<long, PhraseTranslation>>
+                {
+                    ToAdd = [],
+                    ToUpdate = [
+                        new UpdatePair<PhraseTranslation, KeyValuePair<long, PhraseTranslation>>(
+                            new()
                             {
-                                Text = "hassle",
-                                LanguageCode = "en",
+                                Source = new Phrase
+                                {
+                                    Text = "hassle",
+                                    LanguageCode = "en",
+                                },
+                                Target = new Phrase
+                                {
+                                    Text = "хлопоты",
+                                    LanguageCode = "ru",
+                                },
                             },
-                            Target = new Phrase
-                            {
-                                Text = "стычка",
-                                LanguageCode = "ru",
-                            },
-                        })),
-            ],
-            ToDelete = [],
-        };
-        result.Should().BeEquivalentTo(expectedResult);
+                            KeyValuePair.Create(
+                                1465978079469,
+                                new PhraseTranslation
+                                {
+                                    Source = new Phrase
+                                    {
+                                        Text = "hassle",
+                                        LanguageCode = "en",
+                                    },
+                                    Target = new Phrase
+                                    {
+                                        Text = "стычка",
+                                        LanguageCode = "ru",
+                                    },
+                                })),
+                    ],
+                    ToDelete = [],
+                },
+            },
+        ]);
+
+    [Theory]
+    [MemberData(nameof(GetTestCases))]
+    public void CalculateChangeSet_ReturnsEmpty_WhenNoChanges(TestCase testCase)
+    {
+        var result = PhraseTranslationChangeSetCalculator
+            .CalculateChangeSet(testCase.Source, testCase.Target);
+
+        result.Should().BeEquivalentTo(testCase.ExpectedResult);
+    }
+
+    public sealed record TestCase
+    {
+        public required int Id { get; init; }
+
+        public required PhraseTranslation[] Source { get; init; }
+
+        public required IReadOnlyDictionary<long, PhraseTranslation> Target { get; init; }
+
+        public required ChangeSet<PhraseTranslation, KeyValuePair<long, PhraseTranslation>>
+            ExpectedResult { get; init; }
     }
 }
