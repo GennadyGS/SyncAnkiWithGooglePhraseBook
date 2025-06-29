35000b621531a54e3a95990ed09bbe7b9e758ecc
Gennadii Saltyshchak <GennadyGS@gmail.com>
2025-06-29 15:29:51 +0300

    refactor: migrate to await

--- src/src/SyncAnkiWithGooglePhraseBookExtension/wwwroot/scripts/backgroundInterop.js
+++ src/src/SyncAnkiWithGooglePhraseBookExtension/wwwroot/scripts/backgroundInterop.js
@@ -1,20 +1,31 @@
-window.waitForTabToLoad = function (tabId) {
-    return new Promise((resolve) => {
-        function listener(updatedTabId, changeInfo) {
-            if (updatedTabId === tabId && changeInfo.status === "complete") {
-                console.log("Window is loaded")
-                chrome.tabs.onUpdated.removeListener(listener);
-                resolve();
-            }
-        }
-        chrome.tabs.get(tabId, function (tab) {
-            if (tab.status === "complete") {
-                console.log("Window is already loaded")
-                resolve();
-            } else {
-                console.log("Adding tab listener")
-                chrome.tabs.onUpdated.addListener(listener);
-            }
+window.waitForTabToLoad = async function (tabId) {
+    function tabStatus(tabId) {
+        return new Promise((resolve) => {
+            chrome.tabs.get(tabId, function (tab) {
+                resolve(tab.status);
+            });
         });
-    });
+    }
+
+    function waitForComplete(tabId) {
+        return new Promise((resolve) => {
+            function listener(updatedTabId, changeInfo) {
+                if (updatedTabId === tabId && changeInfo.status === "complete") {
+                    console.log("Window is loaded");
+                    chrome.tabs.onUpdated.removeListener(listener);
+                    resolve();
+                }
+            }
+            chrome.tabs.onUpdated.addListener(listener);
+        });
+    }
+
+    const status = await tabStatus(tabId);
+    if (status === "complete") {
+        console.log("Window is already loaded");
+        return;
+    } else {
+        console.log("Adding tab listener");
+        await waitForComplete(tabId);
+    }
 };
