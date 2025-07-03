6b974b1f01dff96d5a6e2f0886bfaf5bdc5d7211
Gennadii Saltyshchak <GennadyGS@gmail.com>
2025-07-03 16:42:57 +0300

    feat: stop script on errors

--- ExportGooglePhrasebookAndSyncAnki.ps1
+++ ExportGooglePhrasebookAndSyncAnki.ps1
@@ -3,7 +3,29 @@ param (
     [switch] ${what-if}
 )
 
+function Invoke-ExternalCommand {
+    param(
+        [Parameter(Mandatory=$true, Position=0)]
+        [string]$Command,
+
+        [Parameter(ValueFromRemainingArguments=$true)]
+        [string[]]$Arguments
+    )
+
+    & $Command @Arguments
+    if ($LastExitCode -ne 0) {
+        throw "Command '$Command' failed with exit code $LastExitCode"
+    }
+}
+
 $ErrorActionPreference = "Stop"
+trap {
+    Write-Host "ERROR: $($_.Exception.Message)" -ForegroundColor Red
+    Write-Host "Press enter to exit..."
+    Read-Host
+    exit 1
+}
+
 if (${what-if}) {
     Write-Host "Running in what-if mode. No changes will be applied."
 }
@@ -20,20 +42,19 @@ $credentialFilePath = "$exportGooglePhraseBookFromSpreadSheetAppPath/credential.
 $credential = Get-Content $credentialFilePath -Raw | ConvertFrom-Json
 
 Push-Location "$appPathRoot/GoogleDriveFileControl"
-dotnet run -- -a Share -i $spreadSheetId -u $credential.client_email
+Invoke-ExternalCommand dotnet run "--" "-a" Share "-i" $spreadSheetId "-u" $credential.client_email
 Pop-Location
 
-dotnet run --project $exportGooglePhraseBookFromSpreadSheetAppPath `
-    -- -i $spreadSheetId -o $phraseBookFileName
+Invoke-ExternalCommand dotnet run "--project" $exportGooglePhraseBookFromSpreadSheetAppPath `
+    "--" "-i" $spreadSheetId "-o" $phraseBookFileName
 
 cmd /c start "" "${Env:LocalAppData}\Programs\Anki\anki.exe"
 
-dotnet run --project "$appPathRoot/UpdateAnki" `
-    -- `
-    -i $phraseBookFileName $(${what-if} ? "--what-if" : "") `
-    -l $logPath
+Invoke-ExternalCommand dotnet run "--project" "$appPathRoot/UpdateAnki" `
+    "--" `
+    "-i" $phraseBookFileName $(${what-if} ? "--what-if" : "") `
+    "-l" $logPath
 
 Push-Location "$appPathRoot/GoogleDriveFileControl"
-dotnet run -- -a Delete -i $spreadSheetId
+Invoke-ExternalCommand dotnet run "--" "-a" Delete "-i" $spreadSheetId
 Pop-Location
-
